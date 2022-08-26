80add239bd71d41df02bdb75d03a429fae77fd23
Denis Skripov <nizhikebinesi@gmail.com>
2022-08-26 19:27:04 +0600

    add structs for GetAd func

--- main.go
+++ main.go
@@ -101,12 +101,12 @@ func (u *UniversalHandler) GetAd(w http.ResponseWriter, r *http.Request) {
 }
 
 type GetAdAPI struct {
-	ID int `json:"id"`
+	ID     int      `json:"id"`
 	Fields []string `json:"fields"`
 }
 
 type GetAdAnswer struct {
-	Status string `json:"status"`
+	Status string                  `json:"status"`
 	Result *map[string]interface{} `json:"result"`
 }
 
