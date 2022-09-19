ea9c1a3e3ca7ae3fcc3f30ffeb99ee2d6cdeef26
Denis Skripov <nizhikebinesi@gmail.com>
2022-09-19 22:12:21 +0600

    Add linters, comments

--- main.go
+++ main.go
@@ -3,25 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/shopspring/decimal"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"io"
-	"net/http"
-	"net/url"
 )
 
 const paginationSize int = 10
 
-type UniversalHandler struct {
+type universalHandler struct {
 	DB        *gorm.DB
 	validator *validator.Validate
 	logger    *zap.Logger
 }
 
+// Pagination contains all information required to paginate records' table
 type Pagination struct {
 	Offset int    `json:"offset"`
 	By     string `json:"by"`
@@ -32,7 +34,7 @@ type Pagination struct {
 //
 // sorting by price/date_created; asc/desc order
 // TODO: add pagination size to params
-func (u *UniversalHandler) ListAds(w http.ResponseWriter, r *http.Request) {
+func (u *universalHandler) ListAds(w http.ResponseWriter, r *http.Request) {
 	result := ListAdsAnswer{Status: "error"}
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -75,7 +77,7 @@ func (u *UniversalHandler) ListAds(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(bs1)
 }
 
-func (u *UniversalHandler) listAds(offset, paginationSize int, by string, asc bool) (resItems []AdAPIListItem, err error) {
+func (u *universalHandler) listAds(offset, paginationSize int, by string, asc bool) (resItems []AdAPIListItem, err error) {
 	items := []*AdAPIListItem{}
 	ascOrDesc := "asc"
 	if !asc {
@@ -98,7 +100,7 @@ func (u *UniversalHandler) listAds(offset, paginationSize int, by string, asc bo
 //
 // required fields: title, price, main_photo_url
 // additional: by parameter `fields`(description, photo_urls)
-func (u *UniversalHandler) GetAd(w http.ResponseWriter, r *http.Request) {
+func (u *universalHandler) GetAd(w http.ResponseWriter, r *http.Request) {
 	result := GetAdAnswer{Status: "error"}
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -126,7 +128,7 @@ func (u *UniversalHandler) GetAd(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	item, err := u.getAd(api.ID, api.Fields)
+	item, err := u.getAd(api.ID)
 	if err != nil && err != gorm.ErrRecordNotFound {
 		u.logger.Error("error during getAd in GetAd", zap.Error(err))
 		w.WriteHeader(http.StatusInternalServerError)
@@ -141,7 +143,7 @@ func (u *UniversalHandler) GetAd(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// TODO: add item handling
-	//fmt.Println(item)
+	// fmt.Println(item)
 	bs1, _ := json.Marshal(result)
 	_, _ = w.Write(bs1)
 }
@@ -152,10 +154,10 @@ func createMapFromAdItem(item AdItem, fields []string) (m map[string]interface{}
 	m["title"] = item.Title
 	var url *string = nil
 	if item.MainImageURL != nil {
-		//fmt.Println(item)
+		// fmt.Println(item)
 		url = &item.MainImageURL.URL
 	}
-	//fmt.Println(item)
+	// fmt.Println(item)
 	m["main_image_url"] = url
 	for _, field := range fields {
 		if field == "description" {
@@ -171,17 +173,20 @@ func createMapFromAdItem(item AdItem, fields []string) (m map[string]interface{}
 	return m
 }
 
+// GetAdAPI stores information about interesting item and which fields to show
 type GetAdAPI struct {
 	ID     int      `json:"id"`
 	Fields []string `json:"fields"`
 }
 
+// GetAdAnswer couples a status of processing and a request's result
 type GetAdAnswer struct {
 	Status string                  `json:"status"`
 	Result *map[string]interface{} `json:"result"`
 }
 
-func (u *UniversalHandler) getAd(id int, fields []string) (res *AdItem, err error) {
+func (u *universalHandler) getAd(id int) (res *AdItem, err error) {
+	// TODO: add fields to use in .Select(fields)
 	var item AdItem
 	db := u.DB.Preload("ImageURLs").Preload("MainImageURL").First(&item, id).Association("MainImageURL")
 	err = db.Error
@@ -196,7 +201,7 @@ func (u *UniversalHandler) getAd(id int, fields []string) (res *AdItem, err erro
 //
 // Params: title, description, photo_urls, price
 // Return: ID of new ad, code of a result
-func (u *UniversalHandler) CreateAd(w http.ResponseWriter, r *http.Request) {
+func (u *universalHandler) CreateAd(w http.ResponseWriter, r *http.Request) {
 	result := CreateAdAnswer{ID: nil, Status: "error"}
 	if r.Method != "POST" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -231,7 +236,7 @@ func (u *UniversalHandler) CreateAd(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write(bs1)
 		return
 	}
-	//for _, e := range err.(validator.ValidationErrors) {
+	// for _, e := range err.(validator.ValidationErrors) {
 	//	fmt.Println(e)
 	//}
 
@@ -250,22 +255,27 @@ func (u *UniversalHandler) CreateAd(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(bs1)
 }
 
+// CreateAdAnswer stores a status of created item
+// and an ID of created item(if it was created)
 type CreateAdAnswer struct {
 	Status string `json:"status"`
 	ID     *int   `json:"id"`
 }
 
+// ListAdsAnswer combine a status of a list process and a result
 type ListAdsAnswer struct {
 	Status string          `json:"status"`
 	Result []AdAPIListItem `json:"result"`
 }
 
+// ImageURL is a table for image URLs for items
 type ImageURL struct {
 	gorm.Model
 	URL      string
 	AdItemID int
 }
 
+// AdJSONItem contains the information to write to AdItem table
 type AdJSONItem struct {
 	Title       string          `json:"title" validate:"required,min=1,max=200"`
 	Description string          `json:"description" validate:"required,max=1000"`
@@ -273,6 +283,7 @@ type AdJSONItem struct {
 	ImageURLs   []string        `json:"imageURLs" validate:"required,max=3,checkURL"`
 }
 
+// AdItem is a table which contains information about all items
 type AdItem struct {
 	gorm.Model
 	ID           int `sql:"AUTO_INCREMENT" gorm:"primary_key"`
@@ -283,13 +294,14 @@ type AdItem struct {
 	MainImageURL *ImageURL       `gorm:"foreignKey:AdItemID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
+// AdAPIListItem stores information is needed for pagination show
 type AdAPIListItem struct {
 	ID    int
 	Title string
 	Price decimal.Decimal
 }
 
-func (u *UniversalHandler) createAd(item AdJSONItem) (id int, err error) {
+func (u *universalHandler) createAd(item AdJSONItem) (id int, err error) {
 	var mainImageURL *ImageURL = nil
 	size := 0
 	imgURLsSize := len(item.ImageURLs)
@@ -346,41 +358,46 @@ func main() {
 	})
 
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	//c := zap.NewDevelopmentConfig()
-	//c.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	//l, _ := c.Build()
-	//defer l.Sync()
-	//loggerG := zapgorm2.New(l)
-	//loggerG.SetAsDefault() // optional: configure gorm to use this zapgorm.Logger for callbacks
+	// c := zap.NewDevelopmentConfig()
+	// c.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	// l, _ := c.Build()
+	// defer l.Sync()
+	// loggerG := zapgorm2.New(l)
+	// loggerG.SetAsDefault() // optional: configure gorm to use this zapgorm.Logger for callbacks
 
 	dsn := "host=postgres user=gorm password=gorm dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	//db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: loggerG})
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: loggerG})
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		//Logger: glogger.Default.LogMode(glogger.Warn),
 	})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	db.AutoMigrate(&AdItem{}, &ImageURL{})
+	err = db.AutoMigrate(&AdItem{}, &ImageURL{})
+	if err != nil {
+		panic("failed to automigrate")
+	}
 
 	config := zap.NewDevelopmentConfig()
 	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	logger, _ := config.Build()
 
-	//logger, _ := zap.NewDevelopment()
-	defer logger.Sync()
+	// logger, _ := zap.NewDevelopment()
+	defer func() {
+		_ = logger.Sync()
+	}()
 
-	//d, err := db.DB()
+	// d, err := db.DB()
 	////d.Close()
-	//d.Ping()
-	//if err != nil {
+	// d.Ping()
+	// if err != nil {
 	//	fmt.Println(err)
 	//}
 
-	logic := UniversalHandler{DB: db, validator: v, logger: logger}
+	logic := universalHandler{DB: db, validator: v, logger: logger}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/create_ad", logic.CreateAd)
