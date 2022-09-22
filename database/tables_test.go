40d31e685bae3af16270e07ab4df63a0549e1c84
Denis Skripov <nizhikebinesi@gmail.com>
2022-09-22 18:18:51 +0600

    Add price to return params of an item

--- database/tables_test.go
+++ database/tables_test.go
@@ -1,22 +1,43 @@
 package database
 
-import "testing"
+import (
+	"github.com/shopspring/decimal"
+	"reflect"
+	"testing"
+)
 
 func TestCreateMapFromAdItemEmptyItem(t *testing.T) {
 	item := AdItem{}
 	var url *string
-	expected := map[string]interface{}{"id": 0, "main_image_url": url, "title": ""}
+	expected := map[string]interface{}{"id": 0, "main_image_url": url, "title": "", "price": decimal.NewFromInt(0)}
 	actual := item.CreateMap([]string{})
 
 	if len(expected) != len(actual) {
 		t.Fatalf("len(expected) != len(actual) ; expected = %v ; actual = %v", expected, actual)
 	}
+	//if !reflect.DeepEqual(expected, actual) {
+	//	t.Fatalf("expected != actual ; expected = %v ; actual = %v", expected, actual)
+	//}
 	for k, v := range expected {
 		val, ok := actual[k]
 		if !ok {
 			t.Fatalf("k = %s not found in actual ; expected = %v ; actual = %v", k, expected, actual)
 		}
-		if v == nil && val != nil || v != nil && v != val {
+		if k == "price" {
+			r1, ok1 := val.(decimal.Decimal)
+			r2, ok2 := v.(decimal.Decimal)
+			if !ok1 {
+				t.Fatalf("expected[price] does not convert to decimal.Decimal ; expected = %v", expected)
+			}
+			if !ok2 {
+				t.Fatalf("actual[price] does not convert to decimal.Decimal ; actual = %v", actual)
+			}
+			if !r1.Equal(r2) {
+				t.Fatalf("expected[%s] != actual[%s] ; %v != %v ; expected = %v ; actual = %v", k, k, v, val, expected, actual)
+			}
+			continue
+		}
+		if v == nil && val != nil || v != nil && !reflect.DeepEqual(v, val) {
 			t.Fatalf("expected[%s] != actual[%s] ; %v != %v ; expected = %v ; actual = %v", k, k, v, val, expected, actual)
 		}
 	}
@@ -27,7 +48,7 @@ func TestCreateMapFromAdItemNotNilMainImage(t *testing.T) {
 	var url = &s
 	mainURL := ImageURL{URL: *url}
 	item := AdItem{MainImageURL: &mainURL}
-	expected := map[string]interface{}{"id": 0, "main_image_url": url, "title": ""}
+	expected := map[string]interface{}{"id": 0, "main_image_url": url, "title": "", "price": decimal.NewFromInt(0)}
 	actual := item.CreateMap([]string{})
 
 	if len(expected) != len(actual) {
@@ -37,6 +58,20 @@ func TestCreateMapFromAdItemNotNilMainImage(t *testing.T) {
 		val, ok := actual[k]
 		if !ok {
 			t.Fatalf("k = %s not found in actual ; expected = %v ; actual = %v", k, expected, actual)
+		}
+		if k == "price" {
+			r1, ok1 := val.(decimal.Decimal)
+			r2, ok2 := v.(decimal.Decimal)
+			if !ok1 {
+				t.Fatalf("expected[price] does not convert to decimal.Decimal ; expected = %v", expected)
+			}
+			if !ok2 {
+				t.Fatalf("actual[price] does not convert to decimal.Decimal ; actual = %v", actual)
+			}
+			if !r1.Equal(r2) {
+				t.Fatalf("expected[%s] != actual[%s] ; %v != %v ; expected = %v ; actual = %v", k, k, v, val, expected, actual)
+			}
+			continue
 		}
 		if r2, ok := val.(*string); ok {
 			r1, ok := v.(*string)
@@ -56,7 +91,7 @@ func TestCreateMapFromAdItemNotNilMainImage(t *testing.T) {
 func TestCreateMapFromAdItemEmptyItemAdditionalFieldsEmptyImageList(t *testing.T) {
 	var url *string
 	item := AdItem{}
-	expected := map[string]interface{}{"id": 0, "main_image_url": url, "title": "", "description": "", "image_urls": []string{}}
+	expected := map[string]interface{}{"id": 0, "main_image_url": url, "title": "", "description": "", "image_urls": []string{}, "price": decimal.NewFromInt(0)}
 	actual := item.CreateMap([]string{"description", "image_urls"})
 
 	if len(expected) != len(actual) {
@@ -66,6 +101,20 @@ func TestCreateMapFromAdItemEmptyItemAdditionalFieldsEmptyImageList(t *testing.T
 		val, ok := actual[k]
 		if !ok {
 			t.Fatalf("k = %s not found in actual ; expected = %v ; actual = %v", k, expected, actual)
+		}
+		if k == "price" {
+			r1, ok1 := val.(decimal.Decimal)
+			r2, ok2 := v.(decimal.Decimal)
+			if !ok1 {
+				t.Fatalf("expected[price] does not convert to decimal.Decimal ; expected = %v", expected)
+			}
+			if !ok2 {
+				t.Fatalf("actual[price] does not convert to decimal.Decimal ; actual = %v", actual)
+			}
+			if !r1.Equal(r2) {
+				t.Fatalf("expected[%s] != actual[%s] ; %v != %v ; expected = %v ; actual = %v", k, k, v, val, expected, actual)
+			}
+			continue
 		}
 		if k == "image_urls" {
 			r1, ok1 := v.([]string)
