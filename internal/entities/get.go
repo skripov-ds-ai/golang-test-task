db9a596a6ca958b092c46ae84e83cfc6e71ba260
Denis Skripov <nizhikebinesi@gmail.com>
2022-10-05 04:13:16 +0600

    Add singleflight, connection pool, easyjson improvements

--- internal/entities/get.go
+++ internal/entities/get.go
@@ -1,7 +1,19 @@
 package entities
 
+import "github.com/shopspring/decimal"
+
 // GetAdAnswer couples a status of processing and a request's result
-type GetAdAnswer struct {
-	Status string                  `json:"status"`
-	Result *map[string]interface{} `json:"result"`
-}
+type (
+	GetAdAnswer struct {
+		Status string     `json:"status"`
+		Result *APIAdItem `json:"result"`
+	}
+	APIAdItem struct {
+		ID           int             `json:"id"`
+		Title        string          `json:"title"`
+		Price        decimal.Decimal `json:"price"`
+		MainImageURL *string         `json:"main_image_url"`
+		Description  string          `json:"description,omitempty"`
+		ImageURLs    []string        `json:"image_urls,omitempty"`
+	}
+)
--- internal/entities/list.go
+++ internal/entities/list.go
@@ -1,5 +1,7 @@
 package entities
 
+import "github.com/shopspring/decimal"
+
 // PaginationSize is a constant for item list
 const (
 	PaginationSize int    = 10
@@ -8,7 +10,15 @@ const (
 )
 
 // ListAdsAnswer combine a status of a list process and a result
-type ListAdsAnswer struct {
-	Status string                   `json:"status"`
-	Result []map[string]interface{} `json:"result"`
-}
+type (
+	ListAdsAnswer struct {
+		Status string          `json:"status"`
+		Result []APIAdListItem `json:"result"`
+	}
+	APIAdListItem struct {
+		ID           int             `json:"id"`
+		Title        string          `json:"title"`
+		Price        decimal.Decimal `json:"price"`
+		MainImageURL *string         `json:"main_image_url"`
+	}
+)
