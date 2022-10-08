e1a34099ed8ed5a19aa22dd8d23be8d5bcbbe3b6
Denis Skripov <nizhikebinesi@gmail.com>
2022-10-09 04:07:22 +0600

    add draft code for redis cache

---
+++ internal/cache/client.go
@@ -0,0 +1,29 @@
+package cache
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// RedisClient is for wrapping original redis.Client
+type RedisClient struct {
+	redis.Client
+	MaxRetries int
+}
+
+// NewRedisClient is constructor for RedisClient
+func NewRedisClient(ctx context.Context, config RedisConfig) *RedisClient {
+	options := redis.Options{
+		Addr:     config.Addr,
+		Password: config.Password,
+		DB:       config.DB,
+	}
+	client := redis.NewClient(&options)
+
+	if _, err := client.Ping(ctx).Result(); err != nil {
+		panic(err)
+	}
+	maxRetries := 10
+	return &RedisClient{Client: *client, MaxRetries: maxRetries}
+}
